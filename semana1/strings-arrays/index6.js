71d664c85a25b9820aed657d9119b85b68ff103c
Laura Laurena <lauralaurena0204@gmail.com>
2021-08-11 16:01:34 -0300

    strings-e-arrays-javaScript

---
+++ semana1/strings-arrays/index6.js
@@ -0,0 +1,59 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+//EXERCICIO 1--------------------------------
+// let array
+// console.log('a. ', array) // a. undefined
+
+// array = null
+// console.log('b. ', array) // b. null
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length) // c. 11
+
+// let i = 0
+// console.log('d. ', array[i]) // d. 3
+
+// array[i+1] = 19
+// console.log('e. ', array) // e. (11) 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13
+
+// const valor = array[i+6]  // f. 9
+// console.log('f. ', valor)
+
+// //EXERCICIO 2--------------------------------
+// const frase = prompt("Digite uma frase")
+
+// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length) // SUBI NO ONIBUS EM MIRROCOS 26
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO
+// EXERCÍCIO 1 ----------------------------------
+// const nome= prompt ("insira seu nome")
+// const email= prompt ("insira seu email")
+
+// const frase= `O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o) ${nome}!`
+// console.log (frase)
+
+// EXERCÍCIO 2 -----------------------------------
+// let listaDecomidas = ["pizza", "hamburguer", "comida japonesa", "frango assado", "torta de limão" ]
+// console.log ("Essas são minhas comidas favoritas: " + listaDecomidas[0] )
+// console.log ( listaDecomidas[1] )
+// console.log ( listaDecomidas[2] )
+// console.log ( listaDecomidas[3] )
+// console.log ( listaDecomidas[4].pop )
+// const comidaUsuario = prompt ("insira sua comida favorita aqui:")
+// console.log (comidaUsuario)
+
+// EXERCICIO 3 -------------------------------------
+// Array=[]
+// let listaDeTarefas = Array
+// const tarefa1 = prompt ("insira sua tarefa a cumprir")
+// const tarefa2 = prompt ("insira sua segunda tarefa a cumprir")
+// const tarefa3 = prompt ("insira sua terceira tarefa a cumprir")
+// let listaDeTarefasTotais= Array  
+// listaDeTarefasTotais = tarefa1 + tarefa2 + tarefa3 
+// console.log (tarefa1)
+// console.log (tarefa2) 
+// console.log (tarefa3)
+// const retirarTarefa = Number (prompt ("retire o indice de uma tarefa já cumprida (de 0 a 2)"))
+// listaDeTarefasTotais [retirarTarefa]
+
+// não consegui retirar uma das tarefas :((
