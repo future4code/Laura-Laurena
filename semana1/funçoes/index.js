a4e6fda07926c060244df7ccb69e2c2418cbe0b1
Laura Laurena <lauralaurena0204@gmail.com>
2021-08-12 16:35:33 -0300

    funçoes-javaScript

---
+++ semana1/funçoes/index.js
@@ -0,0 +1,148 @@
+// function imprmirOlarMundo(){
+//     console.log ("ola mundo!")
+// }
+
+// imprmirOlarMundo()
+
+// const imprimiNome =function (nome){
+//     console.log ("olá", nome)
+// } 
+// imprimiNome("Laura")
+// imprimiNome("Lucas")
+// imprimiNome("Gabriel")
+// const nomePromp= prompt ("manda um nome ae!")
+// imprimiNome(nomePromp)
+ 
+// const somaNumeros = ( numero1, numero2) => {
+//     const soma = numero1 + numero2
+//     return soma
+// }
+
+// console.log (somaNumeros (1,2))
+
+// function pega1eUltimodivp2(arraydenum) {
+//     const resultado = []
+//     resultado.push(arraydenum[0] / 2)
+//     resultado.push (arraydenum[arraydenum.length - 1] / 2)
+//     return resultado
+// }
+//  console.log (pega1eUltimodivp2([5, 3, 7, 9]))
+
+//EXERCICIOS DE INTERPRETAÇÃO DE CÓDIGO-----------------------------------------
+//------------------1----------------------------
+// function minhaFuncao(variavel) {
+// 	return variavel * 5
+// }
+
+// console.log(minhaFuncao(2))
+// console.log(minhaFuncao(10))
+// A) 10 e 50
+// B) da certo mas não imprime nada no console.
+
+//------------------2------------------------------
+// let textoDoUsuario = prompt("Insira um texto");
+
+// const outraFuncao = function(texto) {
+// 	return texto.toLowerCase().includes("cenoura")
+// }
+
+// const resposta = outraFuncao(textoDoUsuario)
+// console.log(resposta)
+// // A) Ela diminui o texto e pergunta se contém "cenoura"
+// // B) I. true
+// //    II.true
+// //    III.true
+
+//EXERCICIOS DE ESCRITA DE CÓDIGO--------------------------------------------------
+//--------------------1-----------------------------
+// // A) 
+// function dadosSobreMim (dados) {
+// dados= "eu sou a Laura, tenho 18 anos, moro em são paulo e sou estudante."
+// return dados
+// }
+// console.log (dadosSobreMim())
+
+// B)
+// function dadosSobreMim2 (nome, idade, cidade, profissão) {
+//     nome= "laura"
+//     idade="18" 
+//     cidade="São Paulo" 
+//     profissão="estudante"
+//     const frase =`eu sou a ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissão}.`
+//      return frase
+// }
+// console.log (dadosSobreMim2())
+
+// //--------------------2------------------------------
+// // A)
+// function somaNumeros(numero1, numero2) {
+//     return soma = numero1+ numero2
+// }     
+//     console.log (somaNumeros(4,5))
+ 
+// // B)
+// function recebeNumeros(numero1, numero2) {
+//     return maior = numero1 >= numero2
+// }     
+//     console.log (recebeNumeros(4,5)) 
+// // C) 
+// function recebeUmNumero (numero) {
+//     Number (numero) 
+//     return parOuImpar = numero %2===0 
+// }     
+//     console.log (recebeUmNumero (4))
+// // D) 
+// function recebeUmaMensagem (mensagem) {
+//     mensagem = "aqui vai a mensagem!".toUpperCase()
+//     return mensagem
+// }     
+//     console.log (recebeUmaMensagem() )  
+    
+//---------------------3-----------------------------------
+// const insiraAqui= Number (prompt ("insira um numero aqui")) 
+// const insiraAqui2= Number (prompt ("insira outro numero aqui"))
+
+// function soma(numero, numero2){
+//     const somaNumero=  numero + numero2
+//     return somaNumero
+// }          
+// function subtração (numero, numero2){
+//     const subtraçaoNumero=  numero - numero2
+//     return subtraçaoNumero
+// }          
+// function multiplicação (numero, numero2){
+//     const MultiplicaçãoNumero=  numero * numero2
+//     return MultiplicaçãoNumero
+// }                 
+//  function divisão (numero, numero2){
+//     const DivisãoNumero=  numero / numero2
+//     return DivisãoNumero 
+// } 
+// console.log (soma(insiraAqui, insiraAqui2))
+// console.log (subtração(insiraAqui, insiraAqui2))
+// console.log (multiplicação(insiraAqui, insiraAqui2))
+// console.log (divisão(insiraAqui, insiraAqui2))
+
+//-------------------DESAFIOS----------------------------------------------------
+//--------------------1----------------------
+// // A) 
+// const função =(parametro) => {
+// parametro = "eu sou a Laura"
+// return parametro
+// }
+// console.log (função())
+
+// // B)
+// const função2 =(parametro, parametro2) => {
+//     parametro = "eu sou a Laura"
+//     parametro2 = "e tenho 18 anos"
+
+    
+//     function mandar (resultadosoma) {
+//         resultadosoma= parametro + parametro2
+         
+//         console.log (mandar())
+//     }
+
+//     }
+//     console.log (função2 ())
