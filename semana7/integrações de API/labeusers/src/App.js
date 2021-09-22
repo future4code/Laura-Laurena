555b288178c5b9d2ee0f722685ea8b6d3d8b8844
Laura Laurena <lauralaurena0204@gmail.com>
2021-09-22 09:23:16 -0300

    Labeusers atualizado

--- semana7/integrações de API/labeusers/src/App.js
+++ semana7/integrações de API/labeusers/src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from'axios';
+import React from 'react'; 
 
-function App() {
+const headers = {
+  headers: {
+    Authorization: "Laura-Laurena-Maryam"
+  }
+};
+
+export default class App extends React.Component {
+  state={
+      UsuarioName: "",
+      UsuarioEmail: ""
+  };
+
+  componentDidMount() {
+    this.getAllUsuarios();
+  }
+
+  handleUsuarioName = (e) => {
+    this.setState({ UsuarioName: e.target.value });
+  };
+  handleUsuarioEmail = (e) => {
+    this.setState({ UsuarioEmail: e.target.value });
+  };
+
+  createUsuario = () => {
+    const url =
+      "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
+
+    const body = {
+      name: this.state.UsuarioName,
+      email: this.state.UsuarioEmail
+    };
+
+    axios
+      .post(url, body, headers)
+      .then((res) => {
+        this.setState({ UsuarioName: "" });
+        this.getAllUsuarios();
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+  };
+
+  getAllPlaylists = () => {
+    const url =
+      "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
+    axios
+      .get(url, headers)
+      .then((res) => {
+        this.setState({ UsuarioName: res.data.result.list });
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
+
+  render(){
+    const UsuarioComponents = this.state.UsuarioName.map((usuario) => {
+      return <li key={usuario.id}>{usuario.UsuarioName}</li>;
+    });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Labeuser</h1>
+        <input
+          placeholder="Nome do usuario"
+          value={this.state.UsuarioName}
+          onChange={this.handleUsuarioName}
+        />
+        <input
+          placeholder="Email do usuario"
+          value={this.state.UsuarioEmail}
+          onChange={this.handleUsuarioEmail}
+        />
+        <button onClick={this.createUsuario}>Enviar</button>
+        {UsuarioComponents}
     </div>
   );
+  }
 }
 
-export default App;
