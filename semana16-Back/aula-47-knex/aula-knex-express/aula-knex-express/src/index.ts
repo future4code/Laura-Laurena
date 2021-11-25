ac5366e5e63184ea41065c372ccad60f58f7d211
Laura Laurena <lauralaurena0204@gmail.com>
2021-11-25 17:31:23 -0300

    exercicios SQL e knex

--- semana16-Back/aula-47-knex/aula-knex-express/aula-knex-express/src/index.ts
+++ semana16-Back/aula-47-knex/aula-knex-express/aula-knex-express/src/index.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express"
+import connection from "./connection"
+
 
 // Esse arquivo seria o index.ts
 
@@ -34,8 +36,126 @@ app.get("/users/:id", async (req: Request, res: Response) => {
     console.log(await getActorById(id))
 
     res.end()
-  } catch (error) {
-		console.log(error.message)
+  } catch (err) {
+		console.log(err.message)
     res.status(500).send("Unexpected error")
   }
 }) // bata no http://localhost:3003/users/001 e veja o que acontece no terminal
+
+//-----------------1-------------------
+//A) ele nos devolve dois arrays, um com a informações que 
+//nós queremos e outro com informações que não serão na maior 
+//parte dos casos util para o usuario da api 
+//B)
+const searchActor = async (name: string): Promise<any> => {
+  const result = await connection.raw(`
+    SELECT * FROM Actor WHERE name = "${name}"
+  `)
+  return result
+}
+//C)
+const countActors = async (gender: string): Promise<any> => {
+  const result = await connection.raw(`
+  SELECT COUNT(*) FROM Actor WHERE gender = "${gender}"
+`);
+const count = result[0][0].count;
+return count;
+};
+//-----------------2-------------------
+//A)
+const updateActor = async (id: string, salary: number): Promise<any> => {
+  await connection("Actor")
+    .update({
+      salary: salary,
+    })
+    .where("id", id);
+};
+//B)
+const deleteActor = async (id: string): Promise<void> => {
+  await connection("Actor")
+    .delete()
+    .where("id", id);
+}; 
+//C)
+const avgSalary = async (gender: string): Promise<any> => {
+  const result = await connection("Actor")
+    .avg("salary as average")
+    .where({ gender });
+
+  return result[0].average;
+};
+//-----------------3-------------------
+app.get("/actor", async (req: Request, res: Response) => {
+  try {
+    const count = await countActors(req.query.gender as string);
+    res.status(200).send({
+      quantity: count,
+    });
+  } catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
+//-----------------4-------------------
+//A)
+app.put("/actor", async (req: Request, res: Response) => {
+  try {
+    await updateSalary(req.body.id, req.body.salary);
+    res.status(200).send({
+      message: "Success",
+    });
+  } catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
+//B)
+app.delete("/actor/:id", async (req: Request, res: Response) => {
+  try {
+    await deleteActor(req.params.id);
+  } catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
+//-----------------5-------------------
+const createMovie = async (
+  id: string,
+  title: string,
+  synopsis: string,
+  releaseDate: Date,
+  playingLimitDate: Date
+) => {
+  await connection
+    .insert({
+      id: id,
+      title: title,
+      synopsis: synopsis,
+      releas_date: releaseDate,
+      playing_limit_date: playingLimitDate,
+    })
+    .into("Movie");
+};
+
+app.post("/movie", async (req: Request, res: Response) => {
+  try {
+    await createMovie(
+      req.body.id,
+      req.body.title,
+      req.body.synopsis,
+      req.body.releaseDate,
+      req.body.playingLimitDate
+    );
+
+    res.status(200).send({
+      message: "Success"
+    });
+  } catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
